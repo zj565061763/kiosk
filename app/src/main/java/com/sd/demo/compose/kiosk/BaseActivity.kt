32d83f81bf575005a0cb889e2732eba9b78e0640
zjun <565061763@qq.com>
2026-07-08 13:33:41 +0800

    sample

--- app/src/main/java/com/sd/demo/compose/kiosk/BaseActivity.kt
+++ app/src/main/java/com/sd/demo/compose/kiosk/BaseActivity.kt
@@ -1,11 +1,14 @@
 package com.sd.demo.compose.kiosk
 
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.toArgb
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
@@ -15,8 +18,15 @@ import com.sd.lib.kiosk.KioskHandler
 
 open class BaseActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
-    WindowCompat.setDecorFitsSystemWindows(window, false)
+    enableEdgeToEdge()
     super.onCreate(savedInstanceState)
+  }
+
+  override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+    if (ev?.action == MotionEvent.ACTION_DOWN) {
+      showSystemUI()
+    }
+    return super.dispatchTouchEvent(ev)
   }
 
   /** 设置页面内容 */
@@ -30,15 +40,22 @@ open class BaseActivity : ComponentActivity() {
     AppTheme {
       val isKioskEnabled by KioskHandler.isEnabledFlow.collectAsStateWithLifecycle()
       LaunchedEffect(isKioskEnabled) {
-        val controller = WindowCompat.getInsetsController(window, window.decorView)
-        if (isKioskEnabled) {
-          controller.hide(WindowInsetsCompat.Type.systemBars())
-          controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        } else {
-          controller.show(WindowInsetsCompat.Type.systemBars())
-        }
+        showSystemUI(!isKioskEnabled)
       }
       content()
+    }
+  }
+
+  private fun showSystemUI(show: Boolean = !KioskHandler.isEnabled()) {
+    val controller = WindowCompat.getInsetsController(window, window.decorView)
+    if (show) {
+      controller.show(WindowInsetsCompat.Type.systemBars())
+    } else {
+      controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_DEFAULT
+      controller.hide(WindowInsetsCompat.Type.systemBars())
+      val color = androidx.compose.ui.graphics.Color.Black.copy(0.3f).toArgb()
+      window.statusBarColor = color
+      window.navigationBarColor = color
     }
   }
 }
